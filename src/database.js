b35f60fd8fc3dd6f03653d7e0df4ea0df77f0a0c
iarmolla <ivan.armolla@gmail.com>
2023-03-10 17:40:38 -0300

    changing url

--- src/database.js
+++ src/database.js
@@ -5,9 +5,8 @@ mongoose.set("strictQuery", false);
 // mongoose.connect('mongodb://localhost:27017/guitars')
 
 async function initDb() {
-    const url = MONGO_URL
-    await mongoose.connect(url);
+    const url = 'mongodb+srv://iarmolla:iarmolla@cluster0.fpopr8i.mongodb.net/users?retryWrites=true&w=majority'
+   mongoose.connect(url).then((res) => console.log('Conectado a mongodb')).catch((error) => console.log(error))
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-
 export default initDb
