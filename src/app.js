e7c7eb8d5ff7d47367716cae8d445a451fad88c1
iarmolla <ivan.armolla@gmail.com>
2023-04-12 01:41:56 -0300

    remove spaces

--- src/app.js
+++ src/app.js
@@ -47,7 +47,6 @@ io.on('connection', (socket) => {
         await msg.save()
         socket.to(room).emit('received', room , await Message.find({ room: room }))
     })
-
 })
 
 app.use(messagesRoutes)
